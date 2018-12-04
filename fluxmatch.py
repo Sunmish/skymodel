2b1c0a07351474c0392a412f8e951b69b753aafb
Sunmish <stefanduchesne@gmail.com>
2018-12-04 13:48:10 +0800

    Updates to fluxmatch

--- fluxmatch.py
+++ fluxmatch.py
@@ -4,14 +4,21 @@ import sparse_grid_interpolator
 reload(sparse_grid_interpolator)
 rbf = sparse_grid_interpolator.rbf
 
+from astropy.io import fits
+from astropy.wcs import WCS
+
 from skymodel.fitting import cpowerlaw, powerlaw
 
 import logging
 logging.basicConfig(format="%(levelname)s (%(module)s): %(message)s",
                     level=logging.INFO)
 
+import matplotlib as mpl
+mpl.use("Agg")
+import matplotlib.pyplot as plt
 
-def clip(ratios, indices, sigma=2.):
+
+def sigma_clip(ratios, indices, sigma=2.):
     """
     """
 
@@ -23,8 +30,20 @@ def clip(ratios, indices, sigma=2.):
     return ratios[new_indices], indices[new_indices]
 
 
+def nsrc_cut(table, flux_key, nsrc_max):
+    """
+    """
+
+    if len(table) > nsrc_max:
+        threshold = round(np.sort(table[flux_key])[::-1][nsrc_max-1], 1)
+        table = table[table[flux_key] > threshold]
+
+    return table
+
+
+
 def fluxscale(table, freq, threshold=1., ref_freq=154., spectral_index=-0.77,
-              flux_key="flux"):
+              flux_key="flux", nsrc_max=100):
     """
     """
 
@@ -32,6 +51,9 @@ def fluxscale(table, freq, threshold=1., ref_freq=154., spectral_index=-0.77,
 
     predicted_flux, indices = [], []
     ratios = []
+
+    # table = nsrc_cut(table, flux_key, nsrc_max)
+
 
     for i in range(len(table)):
 
@@ -62,9 +84,10 @@ def fluxscale(table, freq, threshold=1., ref_freq=154., spectral_index=-0.77,
 
     logging.info("Number of calibrators: {}".format(len(predicted_flux)))
 
-    predicted_ratios, predicted_indices = clip(ratios, indices)
+    # predicted_ratios, predicted_indices = sigma_clip(ratios, indices)
+    predicted_ratios, predicted_indices = ratios, indices
 
-    return predicted_ratios, predicted_indices
+    return np.asarray(predicted_ratios), np.asarray(predicted_indices)
 
 
 
@@ -107,6 +130,48 @@ def apply_corrections(image, correction_image, outname):
 
 
 
+def plot(correction_image, pra, pdec, ratios, cmap="cubehelix", 
+         outname=None):
+    """
+    """
+
+    size = (10, 8)
+    axes = [0.15, 0.1, 0.83, 0.78]
+    cbax = [0.15, 0.96, 0.83, 0.03]
+
+    wcs = WCS(fits.getheader(correction_image)).celestial
+
+    fig = plt.figure(figsize=size)
+    ax1 = plt.subplot(111, projection=wcs)
+    ax1.set_position(axes)
+
+    ax1.scatter(pra, pdec, s=150, marker="*", c="green")
+
+    norm = mpl.colors.Normalize(vmin=min(ratios), vmax=max(ratios))
+
+    ax1.imshow(np.squeeze(fits.getdata(correction_image)), cmap=cmap, norm=norm,
+               origin="lower")
+
+
+    colorbar_axis = fig.add_axes(cbax)
+    colorbar = mpl.colorbar.ColorbarBase(colorbar_axis, cmap=cmap, norm=norm,
+                                         orientation="horizontal")
+    colorbar.set_label("Correction factor", 
+                       fontsize=24.,
+                       labelpad=0.,
+                       verticalalignment="top",
+                       horizontalalignment="center")
+    colorbar.ax.tick_params(which="major",
+                            labelsize=20.,
+                            length=5.,
+                            color="black",
+                            labelcolor="black",
+                            width=1.)
+
+    if outname is None:
+        outname = correction_image.replace(".fits", ".eps")
+
+    fig.savefig(outname, dpi=72)
 
 
 
