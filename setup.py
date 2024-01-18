4ca043fa8b0bf47f4f881971492355e13d4fc559
Sunmish <stefanduchesne@gmail.com>
2024-01-18 10:09:17 +0800

    version change

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ scripts = [
 
 setuptools.setup(
     name="skymodel",
-    version="1.2.1",
+    version="1.3.1",
     author="S W Duchesne",
     author_email="stefanduchesne@gmail.com",
     description="A small package to create sky models for MWA calibration.",
